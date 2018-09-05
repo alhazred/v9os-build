11de45a7321991d8cf66452201c331bdc99182ef
Alexander Eremin <alexander.r.eremin@gmail.com>
2018-09-05 17:35:28 +0000

    update tmux to 2.6

--- build/tmux/build.sh
+++ build/tmux/build.sh
@@ -21,20 +21,21 @@
 # CDDL HEADER END
 #
 #
-# Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Copyright 2016 OmniTI Computer Consulting, Inc.  All rights reserved.
 # Use is subject to license terms.
 #
 # Load support functions
 . ../../lib/functions.sh
 
 PROG=tmux
-VER=2.1
+VER=2.6
 VERHUMAN=$VER
 PKG=terminal/tmux
 SUMMARY="terminal multiplexer"
 DESC="$SUMMARY"
-LIBEVENT_VER=2.0.22
+LIBEVENT_VER=2.1.8
 LDIR=libevent-${LIBEVENT_VER}-stable
+XFORM_ARGS+=" -DLIBEVENT=$LIBEVENT_VER"
 
 BUILDARCH=32
 CONFIGURE_OPTS_32="$CONFIGURE_OPTS_32 --bindir=/usr/bin"
@@ -50,10 +51,15 @@ configure32(){
   logcmd ./configure --disable-static --disable-libevent-install || \
     logerr "failed libevent configure"
   logmsg "building a static libevent"
-  # Ewww, we have to patch libevent as well. Change PATCHDIR for now...
+
+  # We have to patch libevent as well. Change PATCHDIR for now...
+  oBUILDDIR=$BUILDDIR
+  BUILDDIR+="/$LDIR"
   PATCHDIR=patches-libevent
   patch_source
   PATCHDIR=patches
+  BUILDDIR=$oBUILDDIR
+
   logcmd make || logerr "failed libevent build"
   popd > /dev/null
   configure32_orig
